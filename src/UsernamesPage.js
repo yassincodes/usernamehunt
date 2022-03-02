c3a38efaeb2801375e6b0277551a8affa98b44c4
yassin <meddyassin@hotmail.com>
2022-03-02 23:50:47 +0100

    .

--- src/UsernamesPage.js
+++ src/UsernamesPage.js
@@ -2,16 +2,17 @@ import {useContext} from "react"
 import {usernamesContext} from "./usernamesContext"
 import HomeNav from "./HomeNav"
 import uuid from "react-uuid"
+import { Link } from "react-router-dom"
 
 function UsernamesPage() {
     const {usernamesArray} = useContext(usernamesContext)
-  
     return (
         <div>
             <HomeNav />
             {usernamesArray.map((username, key) => {
               return <div key={uuid()}>{username.username} {username.inspiredFrom}</div>
             })} 
+        <Link to="/">back to home</Link>
         </div>
     )
 }
