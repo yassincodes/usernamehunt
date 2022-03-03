782fbaa29d572090c9a55c2816eaf4f141c9e709
yassin <meddyassin@hotmail.com>
2022-03-03 02:32:06 +0100

    .

--- src/UsernamesPage.js
+++ src/UsernamesPage.js
@@ -11,10 +11,12 @@ function UsernamesPage() {
         <div>
             <HomeNav />
             <div className="main_section"><HomeForm /></div>
-            <div><span className="box">username</span><span className="box">inspired from</span></div>
-            {usernamesArray.map((username, key) => {
-              return <div key={uuid()}><span className="box">{username.username}</span><span className="box">{username.inspiredFrom}</span></div>
-            })} 
+            <main>
+                <div className="main_box"><span className="box">username</span><span className="box">inspired from</span></div>
+                {usernamesArray.map((username) => {
+                    return <div key={uuid()} className="main_box"><span className="box">{username.username}</span><span className="box">{username.inspiredFrom}</span></div>
+                })} 
+            </main>
         <Link to="/">back to home</Link>
         </div>
     )
