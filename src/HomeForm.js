fb9d977b4b6dd705311f4f6a798f4ebe4cf6bef6
yassin <meddyassin@hotmail.com>
2022-03-07 21:13:31 +0100

    .

--- src/HomeForm.js
+++ src/HomeForm.js
@@ -12,27 +12,37 @@
       // make different array + when u map the big one you will just concat them all
       // before maping . you need to change the state to choose the type of array you want
 
-import React, {useContext, useEffect, useState} from "react"
+
+
+// bugs to fix rn
+// make user type in only proper names
+// prevent default 
+// fix warnings
+import React, {useContext, useEffect} from "react"
 import {usernamesContext} from "./usernamesContext"
 import { Link } from "react-router-dom"
 
 function HomeForm() {
-    const {setArrayNumber,setArrayUsername, firstInput, secondInput, setFirstInput, setSecondInput, array, setUsernamesArray} = useContext(usernamesContext)
+    const {setArrayNumber, firstInput, secondInput, setFirstInput, setSecondInput, array, setUsernamesArray} = useContext(usernamesContext)
     let firstName = ''
     let secondName = ''
 
     function findUsername(firstInput, secondInput) { 
-      firstName = firstInput && firstInput.split('').filter(char => /[a-zA-Z]/.test(char)).join("")
-      secondName = firstInput && secondInput.split('').filter(char => /[a-zA-Z]/.test(char)).join("")
-
-      array[0] = {key: 0, username: firstName + "io", inspiredFrom: "@levelsio"}
-      array[1] = {key: 1, username: "hey" + firstName, inspiredFrom: "@heyeaslo"}
-      array[2] = {key: 2, username: "DH" + firstName, inspiredFrom: "@ddheh"}
-      array[3] = {key: 3, username: secondName + "45785", inspiredFrom: "@someone"}
-      setUsernamesArray(array)
-
-      localStorage.setItem("firstName", firstName)
-      localStorage.setItem("secondName", secondName) 
+        if (firstInput === "n" && secondInput === "n") {
+            firstName = firstInput && firstInput.split('').filter(char => /[a-zA-Z]/.test(char)).join("")
+            secondName = firstInput && secondInput.split('').filter(char => /[a-zA-Z]/.test(char)).join("")
+      
+            array[0] = {key: 0, username: firstName + "io", inspiredFrom: "@levelsio"}
+            array[1] = {key: 1, username: "hey" + firstName, inspiredFrom: "@heyeaslo"}
+            array[2] = {key: 2, username: "DH" + firstName, inspiredFrom: "@ddheh"}
+            array[3] = {key: 3, username: secondName + "45785", inspiredFrom: "@someone"}
+            setUsernamesArray(array)
+      
+            localStorage.setItem("firstName", firstName)
+            localStorage.setItem("secondName", secondName) 
+        } else {
+            alert("go fuck yourself")
+        }
     }  
 
     useEffect(() => {
@@ -49,7 +59,7 @@ function HomeForm() {
         <form>
             <input type="text" id="firstName" value={firstInput === "null" ? "" : firstInput} onChange={(e) => setFirstInput(e.target.value)}/>
             <input type="text" id="lastName"  value={secondInput === "null" ? "" : secondInput} onChange={(e) => setSecondInput(e.target.value)} />
-            <button onClick={fixFindUsername}><Link to="/usernames" id="findUsername" onClick={() => findUsername(firstInput, secondInput)}>find username!</Link></button>
+            <button onClick={fixFindUsername}><Link to={(firstInput === "n" && secondInput === "n") && "/usernames"} id="findUsername" onClick={() => findUsername(firstInput, secondInput)}>find username!</Link></button>
         </form>
     )
 }
