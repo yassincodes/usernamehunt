d52eaafc902b8f82a89b7e3546df15f8ed822282
yassin <meddyassin@hotmail.com>
2022-03-30 22:45:07 +0100

    .

--- src/extension.js
+++ src/extension.js
@@ -1,13 +1,21 @@
 import './extension.css';
 import HomeNav from "./HomeNav"
+import {useState} from "react"
 
 function Extension() {
+    const [isBg, setIsBg] = useState(true)
+
     return (
         <div>
             <HomeNav />
             <div className="extension">
-                <p>download extension</p>
-                <p>send us a username</p>
+                <p className="download" onClick={() => setIsBg(!isBg)} style={isBg ? {background:"black", color: "white"} : {background: "white"}}>download extension</p>
+                <p className="send" onClick={() => setIsBg(!isBg)} style={!isBg ? {background:"black", color: "white"} : {background: "white"}}>send us a username</p>
+            </div>
+            <div className="content">
+                {
+                    isBg ? "hi" : "cool"
+                }
             </div>
         </div>
     )
